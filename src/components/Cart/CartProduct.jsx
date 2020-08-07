3db2eb37fb9a6e048ee3590ad423b61b80909282
Diego Rodriguez <alejandro27062@gmail.com>
2020-08-07 16:35:37 -0500

    improved product item styles

--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../../styles/components/Cart/CartProduct.sass'
-import deleteIcon from '../../assets/icons/delete.svg'
-import minusIcon from '../../assets/icons/minus.svg'
-import plusIcon from '../../assets/icons/plus.svg'
+import deleteIcon from '../../assets/icons/backspace.svg'
+import minusIcon from '../../assets/icons/minus-square.svg'
+import plusIcon from '../../assets/icons/plus-square.svg'
 
 const CartProduct = ({ count, setCount, title, description, image }) => (
   <div className='Cart-product'>
@@ -12,18 +12,17 @@ const CartProduct = ({ count, setCount, title, description, image }) => (
     <div className='Cart-product_desc'>
       <h4>{title}</h4>
       <p>{description}</p>
-      <div className='Cart-product_desc-buttons'>
-        <button type='button' onClick={() => setCount(count > 0 && count - 1)}>
-          <img src={minusIcon} alt='minus' />
-        </button>
-        <span>{count}</span>
-        <button type='button' onClick={() => setCount(count + 1)}>
-          <img src={plusIcon} alt='plus' />
-        </button>
-      </div>
+      <p className='price'>$ 4.99</p>
     </div>
-    <div className='Cart-product_delete'>
-      <button type='button'>
+    <div className='Cart-product_buttons'>
+      <button type='button' onClick={() => setCount(count > 0 && count - 1)}>
+        <img src={minusIcon} alt='minus' />
+      </button>
+      <span>{count}</span>
+      <button type='button' onClick={() => setCount(count + 1)}>
+        <img src={plusIcon} alt='plus' />
+      </button>
+      <button type='button' className='delete'>
         <img src={deleteIcon} alt='delete' />
       </button>
     </div>
